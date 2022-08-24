80c265169cf3cf7b0fd3f8ccc49d5395721a2694
j10c <blyb1739@gmail.com>
2022-08-24 10:36:01 +0800

    fix(lessons-table): 修复课表当前时间指示条定位错误

--- src/components/LessonsTable/index.vue
+++ src/components/LessonsTable/index.vue
@@ -22,7 +22,10 @@
             </view>
           </view>
         </view>
-        <view class="table table-box" v-if="lessonsTable">
+        <view
+          class="table table-box"
+          v-if="lessonsTable && lessonsTable.length !== 0"
+        >
           <view
             class="class"
             v-for="cl in lessonsTable"
@@ -53,112 +56,108 @@
   </view>
 </template>
 
-<script lang="ts">
-  import Taro, { eventCenter, getCurrentInstance } from '@tarojs/taro';
+<script setup lang="ts">
+  import { dayScheduleStartTime } from '@/constants/index';
   import { Lesson } from '@/types/Lesson';
-  import { defineComponent } from 'vue';
+  import { computed, toRefs } from 'vue';
+  import { defineProps, defineEmits } from 'vue';
   import './index.scss';
-  export default defineComponent({
-    props: {
-      lessons: Array,
-      isThisWeek: {
-        type: Boolean,
-        default: false
-      }
-    },
 
-    data() {
-      return {
-        Height: Number,
-        showPop: false
-      };
-    },
-    computed: {
-      weekdayEnum() {
-        return ['周一', '周二', '周三', '周四', '周五', '周六', '周日'];
-      },
-      lessonsTable(): Lesson[] {
-        return this.markConflictLesson(this.lessons);
-      },
-      nowWeekStyle() {
-        const now = new Date();
-        const weekday = now.getDay() ? now.getDay() : 7;
-        const left = `calc(100% / 7 * ${weekday - 1})`;
-        return { left };
-      },
-      nowStyle() {
-        const now = new Date();
-        const hour = now.getHours();
-        const min = now.getMinutes();
-        const rate = ((hour - 8) * 60 + min) / ((21 - 8) * 60 + 5);
-        // const weekday = now.getDay() ? now.getDay() : 7;
-        // const left = 'calc(' + ((weekday - 1) * 126) / 7 + '% + 2rem)';
-        // FIXME:
-        return {
-          top: `calc(${(rate > 0 ? rate : 0) * this.Height}px + 2rem)`
-        };
-      }
-    },
-    mounted() {
-      eventCenter.on(getCurrentInstance()?.router?.onReady || 'onReady', () => {
-        const query = Taro.createSelectorQuery();
-        query.select('.table-box').boundingClientRect();
-        query.exec((res) => {
-          if (typeof res[0].height === 'number')
-            this.Height = Number(res[0].height);
-        });
-      });
-    },
-    methods: {
-      markConflictLesson(lessons: Lesson[]) {
-        if (lessons)
-          for (let i = 0; i < lessons.length; i++) {
-            for (let j = i + 1; j < lessons.length; j++) {
-              const item = lessons[i],
-                item2 = lessons[j];
-              if (item.weekday !== item2.weekday) continue;
-              const from1 = parseInt(item.sections.split('-')[0]);
-              const to1 = parseInt(item.sections.split('-')[1]);
-              const from2 = parseInt(item2.sections.split('-')[0]);
-              const to2 = parseInt(item2.sections.split('-')[1]);
+  const props = defineProps<{ lessons: Lesson[]; isThisWeek: boolean }>();
+  const { lessons } = toRefs(props);
+  const emit = defineEmits(['classClick']);
 
-              if (!(to1 < from2 || to2 < from1)) {
-                lessons[i]['mark'] = true;
-                lessons[j]['mark'] = true;
-              }
-            }
-          }
-        return lessons;
-      },
-      splitNameAndRoom(str: string) {
-        let index = 0;
-        for (; index < str.length; index++) {
-          if (str.charCodeAt(index) <= 255) break;
-        }
-        return [str.slice(0, index), str.slice(index)];
-      },
-      classCardClick(theClass: Lesson) {
-        this.$emit('classClick', theClass);
-      },
-      getDynamicColor(classID = '1') {
-        const index = parseInt(classID.slice(4, 8) + classID.slice(12, 16), 16);
-        const colorSet = ['green', 'cyan', 'blue', 'yellow', 'orange'];
-        return {
-          backgroundColor: `var(--wjh-color-${
-            colorSet[index % colorSet.length]
-          })`
-        };
-      },
-      getPosition(theClass) {
-        const begin = parseInt(theClass.sections.split('-')[0]);
-        const end = parseInt(theClass.sections.split('-')[1]);
-        const weekday = parseInt(theClass.weekday);
-        const fontSize = Math.min(12, (end - begin + 2) * 4) + 'px';
-        const height = `calc(100% / 12 * ${end - begin + 1})`;
-        const top = `calc(100% / 12 * ${begin - 1})`;
-        const left = `calc(100% / 7 * ${weekday - 1})`;
-        return { top, left, height, fontSize };
-      }
-    }
+  const weekdayEnum = computed(() => {
+    return ['周一', '周二', '周三', '周四', '周五', '周六', '周日'];
   });
+  const lessonsTable = computed(() => {
+    return markConflictLesson(lessons?.value);
+  });
+  const nowWeekStyle = computed(() => {
+    const now = new Date();
+    const weekday = now.getDay() ? now.getDay() : 7;
+    const left = `calc(100% / 7 * ${weekday - 1})`;
+    return { left };
+  });
+  const nowStyle = computed(() => {
+    const now = new Date();
+    const hour = now.getHours();
+    const min = now.getMinutes();
+    let duration = 0;
+    const nowTime = hour * 60 + min;
+
+    // 这节课的开始时间
+    let thisLesson = dayScheduleStartTime.find((item) => {
+      if (
+        nowTime >= item.hour * 60 + item.min &&
+        nowTime <= item.hour * 60 + item.min + 45
+      )
+        return true;
+    });
+
+    if (!thisLesson) {
+      thisLesson = dayScheduleStartTime.find((item) => {
+        if (nowTime < item.hour * 60 + item.min) return true;
+      }) || { hour: 21, min: 10 };
+    } else duration = hour * 60 + min - (thisLesson.hour * 60 + thisLesson.min);
+
+    let jc = 0; // 第 ${jc + 1} 节课
+    jc = dayScheduleStartTime.indexOf(thisLesson);
+
+    // TODO:
+    // const weekday = now.getDay() ? now.getDay() : 7;
+    // const left = 'calc(' + ((weekday - 1) * 126) / 7 + '% + 2rem)';
+
+    return {
+      top: `calc((100% - 2rem) / 12 * ${jc + duration / 45} + 2rem)`
+    };
+  });
+
+  function markConflictLesson(lessons: Lesson[]) {
+    if (lessons && lessons.length !== 0)
+      for (let i = 0; i < lessons.length; i++) {
+        for (let j = i + 1; j < lessons.length; j++) {
+          const item = lessons[i],
+            item2 = lessons[j];
+          if (item.weekday !== item2.weekday) continue;
+          const from1 = parseInt(item.sections.split('-')[0]);
+          const to1 = parseInt(item.sections.split('-')[1]);
+          const from2 = parseInt(item2.sections.split('-')[0]);
+          const to2 = parseInt(item2.sections.split('-')[1]);
+
+          if (!(to1 < from2 || to2 < from1)) {
+            lessons[i]['mark'] = true;
+            lessons[j]['mark'] = true;
+          }
+        }
+      }
+    return lessons;
+  }
+  function splitNameAndRoom(str: string) {
+    let index = 0;
+    for (; index < str.length; index++) {
+      if (str.charCodeAt(index) <= 255) break;
+    }
+    return [str.slice(0, index), str.slice(index)];
+  }
+  function classCardClick(theClass: Lesson) {
+    emit('classClick', theClass);
+  }
+  function getDynamicColor(classID = '1') {
+    const index = parseInt(classID.slice(4, 8) + classID.slice(12, 16), 16);
+    const colorSet = ['green', 'cyan', 'blue', 'yellow', 'orange'];
+    return {
+      backgroundColor: `var(--wjh-color-${colorSet[index % colorSet.length]})`
+    };
+  }
+  function getPosition(theClass) {
+    const begin = parseInt(theClass.sections.split('-')[0]);
+    const end = parseInt(theClass.sections.split('-')[1]);
+    const weekday = parseInt(theClass.weekday);
+    const fontSize = Math.min(12, (end - begin + 2) * 4) + 'px';
+    const height = `calc(100% / 12 * ${end - begin + 1})`;
+    const top = `calc(100% / 12 * ${begin - 1})`;
+    const left = `calc(100% / 7 * ${weekday - 1})`;
+    return { top, left, height, fontSize };
+  }
 </script>
---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+import { dayScheduleStartTime } from './dayScheduleStartTime';
+
+export { dayScheduleStartTime };
