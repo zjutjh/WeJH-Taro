66dd0646c5e29212b54d7fecb990d36744db999f
Patrickill <1322301665@qq.com>
2022-12-28 21:44:00 +0800

    先同步远程库

--- src/pages/exam/index.config.ts
+++ src/pages/exam/index.config.ts
@@ -1,6 +1,6 @@
 export default {
-  navigationBarTitleText: "考试信息",
-  backgroundColor: "#dfdfdf",
-  disableScroll: true,
-  navigationStyle: "custom"
+    navigationBarTitleText: "考试信息",
+    backgroundColor: "#dfdfdf",
+    disableScroll: true,
+    navigationStyle: "custom"
 };
--- src/store/system/index.ts
+++ src/store/system/index.ts
@@ -18,33 +18,33 @@ export interface SystemStoreType {
 }
 
 export const SystemStore = {
-  state: () => ({
-    loading: false,
-    generalInfo: {},
-    version: "",
-    questionnaire: {}
-  }),
-  mutations: {
-    startLoading(state: SystemStoreType) {
-      state.loading = true;
-    },
-    stopLoading(state: SystemStoreType) {
-      state.loading = false;
-    },
-    setGeneralInfo(
-      state: SystemStoreType,
-      value: SystemStoreType["generalInfo"]
-    ) {
-      state.generalInfo = value;
-    },
-    setVersion(state: SystemStoreType, value: string) {
-      state.version = value;
-    },
-    setQuestionnaire(
-      state: SystemStoreType,
-      value: SystemStoreType["questionnaire"]
-    ) {
-      state.questionnaire = value;
+    state: () => ({
+        loading: false,
+        generalInfo: {},
+        version: "",
+        questionnaire: {}
+    }),
+    mutations: {
+        startLoading(state: SystemStoreType) {
+            state.loading = true;
+        },
+        stopLoading(state: SystemStoreType) {
+            state.loading = false;
+        },
+        setGeneralInfo(
+            state: SystemStoreType,
+            value: SystemStoreType["generalInfo"]
+        ) {
+            state.generalInfo = value;
+        },
+        setVersion(state: SystemStoreType, value: string) {
+            state.version = value;
+        },
+        setQuestionnaire(
+            state: SystemStoreType,
+            value: SystemStoreType["questionnaire"]
+        ) {
+            state.questionnaire = value;
+        }
     }
-  }
 };
