f8e5925b01a165b3db55d916575236ea141a7864
j10c <blyb1739@gmail.com>
2023-02-23 18:43:57 +0800

    fix(card): fix expose `title` with no ref from props

--- src/components/Card/index.vue
+++ src/components/Card/index.vue
@@ -1,7 +1,10 @@
 <template>
-  <view :class="`wjh-card wjh-card-size-${size}`" :style="style">
-    <view v-if="title" class="wjh-card-header">
-      {{ title }}
+  <view
+    :class="`wjh-card wjh-card-size-${props.size}`"
+    :style="props.style"
+  >
+    <view v-if="props.title" class="wjh-card-header">
+      {{ props.title }}
     </view>
     <view v-if="$slots.header" class="wjh-card-header">
       <slot name="header"></slot>
@@ -16,14 +19,15 @@
 </template>
 
 <script setup lang="ts">
-import { CSSProperties, ref } from "vue";
+import { CSSProperties } from "vue";
 import "./index.scss";
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   title?: string,
   size?: "small" | "middle",
   style?: CSSProperties,
-}>();
-const { title, size = ref("middle"), style } = props;
+}>(), {
+  size: "middle"
+});
 
 </script>
