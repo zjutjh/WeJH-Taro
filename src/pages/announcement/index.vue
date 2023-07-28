37be1f079c40197221578bd67843401b2619096c
Tong030905 <yalelin030905@outlook.com>
2023-07-28 16:43:48 +0800

    fix(theme-config):修复缩进细节

--- src/pages/announcement/index.vue
+++ src/pages/announcement/index.vue
@@ -1,55 +1,55 @@
 <template>>
   <theme-config>
-   <title-bar title="公告" back-button />
-    <scroll-view :scrollY="true">
-      <view class="header-view">
-        <image src="@/assets/photos/announcement.svg"></image>
-      </view>
-      <view class="tab-bar">
-        <text
-          class="tab"
-          :class="currentTab === 'announcement' ? 'active': undefined"
-          @tap="() => handleTabClick('announcement')"
-        > 精弘公告
-        </text>
-        <text
-          class="tab"
-          :class="currentTab === 'information' ? 'active': undefined"
-          @tap="() => handleTabClick('information')"
-        > 校园资讯
-        </text>
-      </view>
-      <view class="flex-column">
-        <template v-if="currentTab === 'announcement'">
-          <card v-if="!announcementList" style="text-align: center">
-            <view>无通知</view>
-          </card>
-          <card
-            class="announcement-card"
-            v-for="(item, index) in announcementList"
-            :key="index"
-            :title="item.title"
-          >
-            <view> {{ item.content.replace(/\\n/g, '\n') }}</view>
-            <template #footer>
-              <view class="time-wrapper">
-                <view>发布于：{{ timeFormat(item.publishTime) }}</view>
-              </view>
-            </template>
-          </card>
-        </template>
-        <template v-else>
-          <card v-if="!informationList.length" style="text-align: center">
-            <view>暂无校园资讯</view>
-          </card>
-          <information-card
-            v-for="item in informationList"
-           :source="item"
-           :key="item.id"
-          />
-        </template>
-      </view>
-    </scroll-view>
+    <title-bar title="公告" back-button />
+      <scroll-view :scrollY="true">
+        <view class="header-view">
+          <image src="@/assets/photos/announcement.svg"></image>
+        </view>
+        <view class="tab-bar">
+         <text
+           class="tab"
+           :class="currentTab === 'announcement' ? 'active': undefined"
+            @tap="() => handleTabClick('announcement')"
+          > 精弘公告
+          </text>
+          <text
+            class="tab"
+            :class="currentTab === 'information' ? 'active': undefined"
+            @tap="() => handleTabClick('information')"
+          > 校园资讯
+          </text>
+        </view>
+        <view class="flex-column">
+          <template v-if="currentTab === 'announcement'">
+            <card v-if="!announcementList" style="text-align: center">
+              <view>无通知</view>
+            </card>
+            <card
+              class="announcement-card"
+              v-for="(item, index) in announcementList"
+              :key="index"
+              :title="item.title"
+            >
+              <view> {{ item.content.replace(/\\n/g, '\n') }}</view>
+              <template #footer>
+                <view class="time-wrapper">
+                  <view>发布于：{{ timeFormat(item.publishTime) }}</view>
+                </view>
+              </template>
+           </card>
+          </template>
+          <template v-else>
+            <card v-if="!informationList.length" style="text-align: center">
+              <view>暂无校园资讯</view>
+            </card>
+            <information-card
+              v-for="item in informationList"
+            :source="item"
+            :key="item.id"
+            />
+          </template>
+        </view>
+      </scroll-view>
   </theme-config>
 </template>
 
