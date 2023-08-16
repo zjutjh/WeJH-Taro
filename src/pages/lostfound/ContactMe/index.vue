1e7708568f400ff38109efaa020b408963b918ac
Wang Tianci <1641523786@qq.com>
2023-08-16 14:16:30 +0800

    perf(theme):修复失物招领help icon主题色同步

--- src/pages/lostfound/ContactMe/index.vue
+++ src/pages/lostfound/ContactMe/index.vue
@@ -5,20 +5,20 @@ import Taro from "@tarojs/taro";
 
 const emit = defineEmits(["showHelp"]);
 const data: Array<{ content: string; extra?: string }> = [
-  { content: "ForYou朝晖QQ: 2716709627" },
-  { content: "ForYou屏峰QQ: 3252819828" },
-  { content: "ForYou莫干山QQ: 2030219390" },
+  { content: "“For You”工程 朝晖校区 QQ: 2716709627" },
+  { content: "“For You”工程 屏峰校区 QQ: 3252819828" },
+  { content: "“For You”工程 莫干山校区 QQ: 2030219390" },
   {
-    content: "事务大厅-朝晖: 综合楼一楼河畔旁",
-    extra: "事务大厅-朝晖\r\n电话: 88320868",
+    content: "学生事务大厅-朝晖: 综合楼一楼河畔旁",
+    extra: "学生事务大厅-朝晖\r\n电话: 88320868",
   },
   {
-    content: "事务大厅-屏峰: 西4和东15楼下",
-    extra: "事务大厅-屏峰\r\n东15电话: 85290858\r\n西4电话:85290880",
+    content: "学生事务大厅-屏峰: 西4和东15楼下",
+    extra: "学生事务大厅-屏峰\r\n东15电话: 85290858\r\n西4电话:85290880",
   },
   {
-    content: "事务大厅-莫干山: 德8德9连廊",
-    extra: "事务大厅-莫干山\r\n电话: (0571) 8881 3551",
+    content: "学生事务大厅-莫干山: 德8德9连廊",
+    extra: "学生事务大厅-莫干山\r\n电话: (0571) 8881 3551",
   },
 ];
 
@@ -44,10 +44,10 @@ function handleTapHelp() {
 <template>
   <view :class="[styles.container, isNewIPhone() ? styles.ios : undefined]">
       <view @tap="handleClick" :class="styles.arrow" />
-      <text @tap="handleClick" >点击查看联系方式："For You"工程、学生事务大厅</text>
-    <view
-      class="quick-view-help-icon iconfont icon-help"
-      @tap="handleTapHelp"
-    />
+      <text @tap="handleClick" >点击查看相关业务服务组织的联系方式</text>
+      <view
+        :class="[styles.helpicon]" class="iconfont icon-help"
+        @tap="handleTapHelp"
+      />
   </view>
 </template>
