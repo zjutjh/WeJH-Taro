cd857395e770caf55138c12c23709a57891d2e45
Tong030905 <yalelin030905@outlook.com>
2023-08-16 17:50:32 +0800

    fix(faq): 修复页面无法渲染的情况

--- src/pages/connect/faq/index.vue
+++ src/pages/connect/faq/index.vue
@@ -1,38 +1,40 @@
 <template>
-  <view class="faq-view background">
-    <title-bar title="常见问题" back-button/>
-    <scroll-view :scroll-y="true">
-      <view class="flex-column">
-        <card
-          v-for="(item, index) in data"
-          :key="index"
-          size="small"
-        >
-          <w-collapse>
-            <w-collapse-panel
-              :title="item.title"
-              arrow
-            >
-              <view class="answer-wrapper">
-                <text
-                  v-for="str in item.content.split('\n')"
-                  :key="str"
-                  class="answer-text"
-                  :userSelect="true"
-                >
-                  {{ str }}
-                </text>
-              </view>
-            </w-collapse-panel>
-          </w-collapse>
-        </card>
-      </view>
-    </scroll-view>
+  <view class="faq-view">
+    <theme-config>
+      <title-bar title="常见问题" back-button/>
+      <scroll-view :scroll-y="true">
+        <view class="flex-column">
+          <card
+            v-for="(item, index) in data"
+            :key="index"
+            size="small"
+          >
+            <w-collapse>
+              <w-collapse-panel
+                :title="item.title"
+                arrow
+              >
+                <view class="answer-wrapper">
+                  <text
+                    v-for="str in item.content.split('\n')"
+                    :key="str"
+                    class="answer-text"
+                    :userSelect="true"
+                  >
+                    {{ str }}
+                  </text>
+                </view>
+              </w-collapse-panel>
+            </w-collapse>
+          </card>
+        </view>
+      </scroll-view>
+    </theme-config>
   </view>
 </template>
 
 <script setup lang="ts">
-import { TitleBar, Card, WCollapsePanel, WCollapse } from "@/components";
+import { TitleBar, Card, WCollapsePanel, WCollapse, ThemeConfig } from "@/components";
 import { faqText } from "@/constants/copywriting";
 import "./index.scss";
 
