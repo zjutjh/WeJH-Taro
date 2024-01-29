9c1725feaa3ef00788fc4d22423a222805bfca64
xixiibn5100 <1584914306@qq.com>
2024-01-29 11:46:43 +0800

    feat:表格状态变更

--- src/pages/suit/information/index.vue
+++ src/pages/suit/information/index.vue
@@ -10,9 +10,9 @@
           <view :class="style.text1" >
             学号
           </view>
-          <view :class="style.text1" v-for="(item, index) in information1" :key="index">
+          <view :class="style.text1" v-for="(item, index) in information1" :key="index" >
             {{ item }}
-            <input v-model="inputData[item]" :class="style.rounded" />
+            <input v-model="inputData[item]" :class="style.rounded" v-if="change"/>
           </view>
           <view :class="style.text2" style="margin-top: 4%;">
             联系
@@ -20,9 +20,10 @@
           <view :class="style.text2" style="margin-bottom: 8%;">
             方式
           </view>
-          <input v-model="inputData[4]" :class="style.rounded2"/>
-    <w-button :class="style.button_cancle" @click="cancel">取消</w-button>
-    <w-button @click="save" :class="style.button_safe">保存</w-button>
+          <input v-model="inputData[4]" :class="style.rounded2" v-if="change"/>
+    <w-button :class="style.button_cancle" @tap="cancel" v-if=change>取消</w-button>
+    <w-button @tap="save" :class="style.button_safe" v-if=change>保存</w-button>
+    <w-button @tap="editor" :class="style.button_editor" v-else>编辑</w-button>
 
         </Card>
       </view>
@@ -36,34 +37,40 @@ import { ref } from "vue";
 import { WButton, Card, ThemeConfig,TitleBar} from "@/components";
 
 const information1 = [ "姓名", "性别", "学院", "寝室"];
-// 数据对象，用于存储输入框的值
+const change =ref(true);
 const inputData = ref({
-  姓名: "",
-  性别: "",
-  学院: "",
-  寝室: "",
-  联系方式:""
+  name: "",
+  gender: "",
+  college: "",
+  dormitory: "",
+  conntact:""
 });
 
+const editor = () => {
+  change.value = true;
+};
+
 const oldData = {
-  姓名: "",
-  性别: "",
-  学院: "",
-  寝室: "",
-  联系方式:""
+  name: "",
+  gender: "",
+  college: "",
+  dormitory: "",
+  conntact:""
 };
 
 // 点击保存按钮的处理函数
 const save = () => {
-  // 在这里保存数据，例如将其发送到服务器或进行其他处理
+  change.value = false;
   console.log("保存的数据:", inputData.value);
 };
 
-// 点击取消按钮的处理函数
+
 const cancel = () => {
-  // 清空数据
-  for (const key in inputData.value) {
-    inputData.value[key] = "";
-  }
+  change.value = false;
+  inputData.value.name = oldData.name;
+  inputData.value.gender = oldData.gender;
+  inputData.value.college = oldData.college;
+  inputData.value.gender = oldData.gender;
+  inputData.value.conntact = oldData.conntact;
 };
 </script>
