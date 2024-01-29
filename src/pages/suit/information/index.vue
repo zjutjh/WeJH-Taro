fa156b289a33b7f7fdcd7a360a5793c3c78ba8e6
xixiibn5100 <1584914306@qq.com>
2024-01-30 00:12:41 +0800

    feat:修改取消编辑

--- src/pages/suit/information/index.vue
+++ src/pages/suit/information/index.vue
@@ -13,14 +13,16 @@
           <view :class="style.text1" v-for="(item, index) in information1" :key="index" >
             {{ item }}
             <input v-model="inputData[item]" :class="style.rounded" v-if="change"/>
+            <text v-else>{{ oldData[item] }}</text>
           </view>
           <view :class="style.text2" style="margin-top: 4%;">
             联系
           </view>
+          <text v-if="!change" style="color: black;margin-left: 3rem;">{{ oldData['联系方式'] }}</text>
           <view :class="style.text2" style="margin-bottom: 8%;">
             方式
           </view>
-          <input v-model="inputData[4]" :class="style.rounded2" v-if="change"/>
+          <input v-model="inputData['联系方式']" :class="style.rounded2" v-if="change"/>
     <w-button :class="style.button_cancle" @tap="cancel" v-if=change>取消</w-button>
     <w-button @tap="save" :class="style.button_safe" v-if=change>保存</w-button>
     <w-button @tap="editor" :class="style.button_editor" v-else>编辑</w-button>
@@ -39,11 +41,11 @@ import { WButton, Card, ThemeConfig,TitleBar} from "@/components";
 const information1 = [ "姓名", "性别", "学院", "寝室"];
 const change =ref(true);
 const inputData = ref({
-  name: "",
-  gender: "",
-  college: "",
-  dormitory: "",
-  conntact:""
+  姓名: "",
+  性别: "",
+  学院: "",
+  寝室: "",
+  联系方式:""
 });
 
 const editor = () => {
@@ -51,11 +53,11 @@ const editor = () => {
 };
 
 const oldData = {
-  name: "",
-  gender: "",
-  college: "",
-  dormitory: "",
-  conntact:""
+  姓名: "wew",
+  性别: "w",
+  学院: "sss",
+  寝室: "sdf",
+  联系方式:"qqqqqqqqq"
 };
 
 // 点击保存按钮的处理函数
@@ -67,10 +69,10 @@ const save = () => {
 
 const cancel = () => {
   change.value = false;
-  inputData.value.name = oldData.name;
-  inputData.value.gender = oldData.gender;
-  inputData.value.college = oldData.college;
-  inputData.value.gender = oldData.gender;
-  inputData.value.conntact = oldData.conntact;
+  inputData.value.姓名 = oldData.姓名;
+  inputData.value.性别 = oldData.性别;
+  inputData.value.学院 = oldData.学院;
+  inputData.value.寝室 = oldData.寝室;
+  inputData.value.联系方式 = oldData.联系方式;
 };
 </script>
