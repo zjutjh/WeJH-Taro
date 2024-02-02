ca7964d28664e715ae6001d028a30074bd51abb5
Wang Tianci <1641523786@qq.com>
2024-02-02 13:53:51 +0800

    perf(faq&info):优化了借用须知和我的信息界面

--- src/pages/suit/information/index.vue
+++ src/pages/suit/information/index.vue
@@ -5,7 +5,8 @@
       <view :class="style.header">
         <image src="@/assets/photos/suitapply-suitInformation.svg"></image>
       </view>
-      <view :class="style.flexColumn">
+
+      <view class="flex-column">
         <Card title="| 我的信息" style="color: var(--wjh-color-primary)">
           <view :class="style.text1"> 学号 </view>
           <view :class="style.text4">{{ nowData.student_id }}</view>
