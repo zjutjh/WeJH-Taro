5b235cb964ee82ccd6b6b89b56f5777c431ebdd6
xixiibn5100 <1584914306@qq.com>
2024-09-29 22:35:37 +0800

    refactor: 重构主题色配置功能

--- src/store/service/theme.ts
+++ src/store/service/theme.ts
@@ -1,12 +1,20 @@
 import { DarkModeTheme, DarkModeOption } from "@/types/DarkMode";
+interface ThemeList {
+  id?: number;
+  name?: string;
+  theme_config?: string;
+  type: string;
+}
 
 export interface ThemeStoreType {
+  hadTheme: ThemeList
   themeMode: string;
   darkMode: DarkModeOption
 }
 
 export const ThemeStore = {
   state: {
+    hadTheme: [],
     themeMode: "green",
     darkMode: {
       mode: "light",
@@ -14,6 +22,9 @@ export const ThemeStore = {
     }
   },
   mutations: {
+    setHadTheme(state: ThemeStoreType, value: ThemeList) {
+      state.hadTheme = value;
+    },
     setThemeMode(state: ThemeStoreType, value: string) {
       state.themeMode = value;
     },
