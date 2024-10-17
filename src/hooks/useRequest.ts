82e4e51f23a3d01ccf15de0e9c02f411d9a92b3f
j10c <blyb1739@gmail.com>
2024-10-17 23:17:31 +0800

    feat(request): new useRequest hook

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,6 +1,6 @@
 
 import Taro from "@tarojs/taro";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, shallowRef } from "vue";
 
 interface RequestConfigType<TData extends TaroGeneral.IAnyObject, TParams> {
   /** 是否手动发起请求 */
@@ -94,3 +94,74 @@ const useRequest = <TData extends TaroGeneral.IAnyObject, TParams>(
 };
 
 export default useRequest;
+
+type UseRequestOptions<Data, Params> = {
+  /** Promise 还未 fulfilled 时，给 `data` 的初始值 */
+  initialData: Data;
+  /** 自动触发请求，以及调用 `run` 函数不传参数时，默认给请求函数传入的参数 */
+  defaultParams?: Params;
+  /** 是否手动触发请求, 默认为 false */
+  manual?: boolean;
+  /** 最短加载时间，默认为 0ms，在有加载动画效果时防止渲染闪屏 */
+  minLoadingTime?: number;
+  /** 在执行 Promise 前，将 `data` 重置为 `initialData`，默认为 false */
+  resetOnRun?: boolean;
+  onSuccess?: (data: Data) => void;
+  onError?: (error: unknown) => void;
+};
+
+const promiseTimeout = (ms: number) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+};
+
+export function useRequestNext<State, Params extends Record<string, any>>(
+  promise: (params?: Params) => Promise<State>,
+  options: UseRequestOptions<State, Params>
+) {
+  const {
+    initialData,
+    defaultParams,
+    manual = false,
+    minLoadingTime = 0,
+    resetOnRun = false,
+    onSuccess,
+    onError
+  } = options ?? {};
+
+  const loading = ref(false);
+  const data = ref(initialData);
+  const error = shallowRef<unknown | undefined>(undefined);
+
+  async function run(params?: Params) {
+    loading.value = true;
+    error.value = undefined;
+    if (resetOnRun) data.value = initialData;
+
+    // 用 `allSettled` 而不用 try-catch 是为了 Delay 也作用到异常状态
+    const delayedPromise = Promise.allSettled([
+      promise(params ?? defaultParams),
+      promiseTimeout(minLoadingTime)
+    ]);
+    const [promiseResult] = await delayedPromise;
+    if (promiseResult.status === "fulfilled") {
+      data.value = promiseResult.value;
+      onSuccess?.(promiseResult.value);
+    } else {
+      error.value = promiseResult.reason;
+      onError?.(promiseResult.reason);
+    }
+
+    loading.value = false;
+  };
+
+  if (!manual) run();
+
+  return {
+    loading,
+    data,
+    error,
+    run
+  };
+}
