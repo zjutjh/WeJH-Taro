88d2e47f497e638fbf1bf717d0824ce930468925
xixiibn5100 <1584914306@qq.com>
2024-10-20 20:10:13 +0800

    refactor(store):使用pinia重构cardStore

--- src/store/service/card.ts
+++ src/store/service/card.ts
@@ -1,37 +1,54 @@
 import { CardConsume } from "@/types/CardConsume";
+import { ref } from "vue";
+import { defineStore } from "pinia";
 
 export interface CardServiceType {
   balance?: number;
   today: CardConsume[];
+  history: CardConsume[];
   updateTime: {
     today?: Date;
+    history?: Date;
     balance?: Date;
   };
 }
-export const CardServiceStore = {
-  state: () => ({
-    balance: 0,
-    history: [],
-    today: [],
-    updateTime: {
-      balance: undefined,
-      history: undefined,
-      today: undefined
-    }
-  }),
 
-  mutations: {
-    setCardBalance(state: CardServiceType, value: number) {
-      state.balance = value;
-      state.updateTime.balance = new Date();
-    },
-    setCardToday(state: CardServiceType, value: CardConsume[]) {
-      if (value !== null) state.today = value;
-      else state.today = [];
-      state.updateTime.today = new Date();
-    },
-    clearCardToday(state: CardServiceType) {
-      state.today = [];
-    }
-  }
-};
+export const useCardServiceStore = defineStore("card", () => {
+  const balance = ref<number>();
+  const history = ref<CardConsume[]>([]);
+  const today = ref<CardConsume[]>([]);
+  const updateTime = ref<{
+    balance?: Date;
+    history?: Date;
+    today?: Date;
+  }>({
+    balance: undefined,
+    history: undefined,
+    today: undefined
+  });
+
+  const setCardBalance = (value: number) => {
+    balance.value = value;
+    updateTime.value.balance = new Date();
+  };
+
+  const setCardToday = (value: CardConsume[]) => {
+    if (value !== null) today.value = value;
+    else today.value = [];
+    updateTime.value.today = new Date();
+  };
+
+  const clearCardToday = () => {
+    today.value = [];
+  };
+
+  return {
+    balance,
+    history,
+    today,
+    updateTime,
+    setCardBalance,
+    setCardToday,
+    clearCardToday
+  };
+});
