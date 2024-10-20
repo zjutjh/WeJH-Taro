5b86273326725160116c45eb76121aabf6351e40
xixiibn5100 <1584914306@qq.com>
2024-10-20 20:11:48 +0800

    feat(store): 添加仓库入口

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,12 @@
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import { useSystemStore } from "./system";
+import { useServiceStore } from "./service";
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
+export {
+  useSystemStore,
+  useServiceStore
+};
