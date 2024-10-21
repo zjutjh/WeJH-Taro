cefb6ae77ecec25611d285978136221c077e8d03
xixiibn5100 <1584914306@qq.com>
2024-10-22 00:49:05 +0800

    fix(lint): 修复lint报错

--- src/utils/camelize.ts
+++ src/utils/camelize.ts
@@ -1,6 +1,6 @@
 import { ToCamelCase } from "@/types/Camelize";
 
-function stringToCamelCase(str:string): string {
+function stringToCamelCase(str: string): string {
   return str.replace(/_([a-z])/g, (_match, letter) => {
     return letter.toUpperCase();
   });
