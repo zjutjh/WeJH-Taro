fa5ca8fe4cd3d1eef55c0e2886dcecec70a5ad9a
j10c <blyb1739@gmail.com>
2024-11-01 22:35:08 +0800

    chore(library): remove unused code

--- src/store/service/library.ts
+++ src/store/service/library.ts
@@ -4,27 +4,6 @@ import { persistedStorage } from "@/utils";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-export const LibraryServiceStore = {
-  state: () => ({
-    history: [],
-    current: [],
-    updateTime: {
-      history: undefined,
-      current: undefined
-    }
-  }),
-  mutations: {
-    setLibraryHistory(state: any, value: Array<object>) {
-      state.history = value;
-      state.updateTime.history = new Date();
-    },
-    setLibraryCurrent(state: any, value: Array<object>) {
-      state.current = value;
-      state.updateTime.current = new Date();
-    }
-  }
-};
-
 const useLibraryStore = defineStore("library", () => {
   const updateTime = ref<string>();
 
