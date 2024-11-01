5a5fcc6aa0dd43debefef327a11001a57f5537bf
j10c <blyb1739@gmail.com>
2024-11-01 17:31:13 +0800

    refactor(score): rewrite in hooks

---
+++ src/store/service/electricity/score/query.ts
@@ -0,0 +1,35 @@
+import useGeneralInfoStore from "@/store/system/generalInfo";
+import { persistedStorage } from "@/utils";
+import { defineStore, storeToRefs } from "pinia";
+import { ref } from "vue";
+
+const useScoreQueryOptionStore = defineStore("score/query", () => {
+  const { info: generalInfo } = storeToRefs(useGeneralInfoStore());
+
+  const term = ref<"上" | "下" | "短">(generalInfo.value.scoreTerm);
+  const year = ref(generalInfo.value.scoreYear);
+  const period = ref<"期中" | "期末">("期末");
+
+  function setOption(value: {
+    term: "上" | "下" | "短";
+    year: string;
+    period: "期中" | "期末";
+  }) {
+    term.value = value.term;
+    year.value = value.year;
+    period.value = value.period;
+  }
+
+  return {
+    term,
+    year,
+    period,
+    setOption
+  };
+}, {
+  persist: {
+    storage: persistedStorage
+  }
+});
+
+export default useScoreQueryOptionStore;
