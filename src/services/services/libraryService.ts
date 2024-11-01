0a277490e79e2b6594a6b4f157fba6ca92464738
j10c <blyb1739@gmail.com>
2024-11-01 16:05:23 +0800

    refactor(library): rewrite in hooks

--- src/services/services/libraryService.ts
+++ src/services/services/libraryService.ts
@@ -1,20 +1,22 @@
+import { request } from "@/utils";
 import { api } from "../api/apiList";
-import { updateDateStateWithSession } from "../utils/updateDateState";
+import { BorrowBooksInfo } from "@/types/BorrowBooksInfo";
 
 export default class LibraryService {
-  static async getLibraryCurrent(): Promise<any> {
-    return updateDateStateWithSession(
-      api.library.current,
-      null,
-      "setLibraryCurrent"
+  // TODO: 图书馆接口重构后补充类型
+  static async getLibraryCurrent() {
+    return request<BorrowBooksInfo[]>(
+      api.library.current, {
+        method: "POST"
+      }
     );
   }
 
-  static async getLibraryHistory(): Promise<any> {
-    return updateDateStateWithSession(
-      api.library.history,
-      null,
-      "setLibraryHistory"
+  static async getLibraryHistory() {
+    return request<BorrowBooksInfo[]>(
+      api.library.history, {
+        method: "POST"
+      }
     );
   }
 }
--- src/store/service/library.ts
+++ src/store/service/library.ts
@@ -1,3 +1,9 @@
+import { useRequestNext } from "@/hooks";
+import { LibraryService } from "@/services";
+import { persistedStorage } from "@/utils";
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
 export const LibraryServiceStore = {
   state: () => ({
     history: [],
@@ -18,3 +24,30 @@ export const LibraryServiceStore = {
     }
   }
 };
+
+const useLibraryStore = defineStore("library", () => {
+  const updateTime = ref<string>();
+
+  // 当前正在借阅
+  const { data: borrowing, error } = useRequestNext(
+    LibraryService.getLibraryCurrent, {
+      initialData: [],
+      onSuccess: () => {
+        updateTime.value = Date().toString();
+      }
+    }
+  );
+
+  return {
+    borrowing,
+    updateTime,
+    error
+  };
+}, {
+  persist: {
+    storage: persistedStorage,
+    pick: ["borrowing", "updateTime"]
+  }
+});
+
+export default useLibraryStore;
