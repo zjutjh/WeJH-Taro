64688e688fcee8a0669aabdc65f068db1591c14d
luolananDR <1525816996@qq.com>
2025-01-19 20:59:30 +0800

    fix: 修复了绩点计算器的bug

--- src/store/service/score.ts
+++ src/store/service/score.ts
@@ -7,7 +7,8 @@ export const ScoreServiceStore = {
   state: {
     readScoreMarks: [], // 所有已读成绩的标记
     findNewScoresTime: undefined,
-    scorePeriod: "期末" // 记录当前成绩页面选中的期中/期末
+    scorePeriod: "期末", // 记录当前成绩页面选中的期中/期末
+    unCalScore: []
   },
 
   mutations: {
