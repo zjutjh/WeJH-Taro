162c36e10b5c4db74d30b5aba1f7b2b96122a6d5
luolananDR <1525816996@qq.com>
2025-01-19 18:57:21 +0800

    feat:绩点计算器的功能

--- src/components/Collapse/Panel.vue
+++ src/components/Collapse/Panel.vue
@@ -1,10 +1,6 @@
 <template>
   <w-list-item
-    :selected="props.selected"
-    :class="[
-      `wjh-collapse-panel-header`,
-      isActive ? `wjh-collapse-panel-header-active` : undefined
-    ]"
+    class="wjh-collapse-panel-header"
     :arrow="props.arrow ? 'down' : undefined"
     @tap="handleClick"
   >
@@ -12,10 +8,7 @@
     <slot name="header" />
   </w-list-item>
   <view
-    :class="[
-      `wjh-collapse-panel-content`,
-      isActive ? `wjh-collapse-panel-content-active` : undefined
-    ]"
+    class="wjh-collapse-panel-content"
     :style="{ maxHeight: isActive ? props.maxHeight : `0` }"
   >
     <w-list-item>
@@ -29,20 +22,18 @@ import { ref, withDefaults } from "vue";
 import WListItem from "../List/ListItem.vue";
 import "./index.scss";
 
-type PropsType = {
-  title?: string;
-  defaltActive?: boolean
-  maxHeight?: string;
-  arrow?: boolean;
-  selected?: boolean;
-};
+  type PropsType = {
+    title?: string;
+    defaltActive?: boolean
+    maxHeight?: string;
+    arrow?: boolean
+  };
 
 const props = withDefaults(defineProps<PropsType>(), {
   title: undefined,
   defaltActive: false,
   maxHeight: "500px",
-  arrow: false,
-  selected: false
+  arrow: false
 });
 
 const isActive = ref(props.defaltActive);
