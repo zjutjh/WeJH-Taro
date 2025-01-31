f5d046974a10efd87bddfc64366d7499e5951f48
j10c <blyb1739@gmail.com>
2025-01-31 20:17:37 +0800

    refactor(generalInfo): rw in use-query

--- src/store/system/generalInfo.ts
+++ src/store/system/generalInfo.ts
@@ -1,36 +1,38 @@
-import useMemorizedRequest from "@/hooks/useMemorizedRequest";
 import { SystemService } from "@/services";
-import { RequestError } from "@/utils";
-import Taro from "@tarojs/taro";
+import { useQuery } from "@tanstack/vue-query";
+import { computed } from "vue";
+
+const INIT_DATA = {
+  defaultTheme: { } as any,
+  fileUrl: "",
+  "is_begin": true,
+  jpgUrl: "",
+  registerTips: "",
+  schoolBusUrl: "",
+  scoreTerm: "上",
+  scoreYear: "2024",
+  term: "上",
+  termStartDate: "2024-09-15",
+  termYear: "2024",
+  time: "",
+  week: 1
+} as const;
 
 function useGeneralInfo() {
-  const { data } = useMemorizedRequest(
-    "system/generalInfo",
-    SystemService.getGeneralInfo, {
-      initialData: {
-        // @ts-expect-error 代码中未使用
-        defaultTheme: undefined,
-        fileUrl: "",
-        "is_begin": true,
-        jpgUrl: "",
-        registerTips: "",
-        schoolBusUrl: "",
-        scoreTerm: "上",
-        scoreYear: "2024",
-        term: "上",
-        termStartDate: "2024-09-15",
-        termYear: "2024",
-        time: "",
-        week: 1
-      },
-      onError: (e) => {
-        if (e instanceof RequestError)
-          Taro.showToast({ title: `获取系统信息失败: ${e.message}`, icon: "error" });
-      }
-    }
-  );
+  const { data } = useQuery({
+    queryKey: ["system", "generalInfo"],
+    queryFn: SystemService.getGeneralInfo
+  });
 
-  return data;
+  const info = computed(() => {
+    if (!data.value) {
+      return INIT_DATA;
+    } else {
+      return data.value;
+    }
+  });
+
+  return info;
 }
 
 export default useGeneralInfo;
