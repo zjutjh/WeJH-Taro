acecea725b904f1124399eee9bd0077b972dbaae
j10c <blyb1739@gmail.com>
2025-01-31 19:19:14 +0800

    refactor(wxProfile): rw in use-query

--- src/hooks/user/wxProfile.ts
+++ src/hooks/user/wxProfile.ts
@@ -1,20 +1,23 @@
 import Taro from "@tarojs/taro";
-import useMemorizedRequest from "../useMemorizedRequest";
+import { useQuery } from "@tanstack/vue-query";
+
+async function wxProfileFetcher() {
+  const res = await Taro.getUserProfile({ desc: "用于获取头像和昵称" });
+  return res.userInfo;
+}
 
 function useUserWxProfile() {
-  const { data: profile, refresh: getProfile } = useMemorizedRequest(
-    "user/wxProfile",
-    () => Taro.getUserProfile({ desc: "用于获取头像和昵称" }).then(res => res.userInfo),
-    {
-      initialData: undefined,
-      revalidateOnMount: false
-    }
-  );
+  const { data: profile, refetch: getProfile } = useQuery({
+    queryKey: ["wxProfile"],
+    queryFn: wxProfileFetcher,
+    staleTime: Infinity,
+    enabled: false
+  });
 
   return {
     profile,
     getProfile
   };
-}
+};
 
 export default useUserWxProfile;
