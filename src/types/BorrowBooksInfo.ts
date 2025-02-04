bc924afb322edbfb03d4e90ee2f327413beb97b6
折乙 <zheyi563@gmail.com>
2025-02-04 19:46:37 +0800

    fix(library): require判定修改;注释标准化

--- src/types/BorrowBooksInfo.ts
+++ src/types/BorrowBooksInfo.ts
@@ -1,9 +1,13 @@
 export interface BorrowBooksInfo {
-  barcode: string;//书的条形码
+  //书的条形码
+  barcode: string;
   loanDate: string;
-  loanId: string;//该次借阅的id(每次借阅都会生成一个id)
+  //该次借阅的id(每次借阅都会生成一个id)
+  loanId: string;
   locationName: string;
-  normReturnDate: string;//规定归还时间
-  returnDate: string;//实际归还时间
+  //规定归还时间
+  normReturnDate: string;
+  //实际归还时间(已归还时才会有)
+  returnDate: string;
   title: string;
 }
