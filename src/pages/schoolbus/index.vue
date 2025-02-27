c23633ff25279f297e63ff91945a3c6c8b4b74c6
RosyrRais <syr050301@qq.com>
2025-02-27 13:06:40 +0800

    fix(schoolbus): 按高保真修改校车首页UI 更新icon.scss

--- src/pages/schoolbus/index.vue
+++ src/pages/schoolbus/index.vue
@@ -1,65 +1,49 @@
 <template>
   <theme-config>
     <title-bar title="校车" back-button />
-    <scroll-view :scroll-y="true">
-      <view class="header-view">
-        <image src="@/assets/photos/suit.svg" />
-        <view class="extra" @tap="showHelp">
-          <view class="icon-wrapper">
-            <view class="extra-icon iconfont icon-announcement" />
-          </view>
-          <view class="label">
-            公告
-          </view>
+    <view class="schoolbus-title-bar">
+      <view class="bus-btn bus-info">
+        <view class="iconfont icon-schoolbus"/>
+        <view class="description">
+          班车信息
         </view>
       </view>
-
-      <view class="flex-column">
-        <w-list @tap="busInfo">
-          <w-list-item arrow="right" class="suit-list-item">
-            <view class="text-wrapper">
-              <text>班车信息</text>
-            </view>
-          </w-list-item>
-        </w-list>
-        <w-list @tap="busTicket">
-          <w-list-item arrow="right" class="suit-list-item">
-            <view class="text-wrapper">
-              <text>已约车票</text>
-            </view>
-          </w-list-item>
-        </w-list>
-        <w-list @tap="busRecord">
-          <w-list-item arrow="right" class="suit-list-item">
-            <view class="text-wrapper">
-              <text>乘车记录</text>
-            </view>
-          </w-list-item>
-        </w-list>
+      <view class="bus-btn bus-ticket">
+        <view class="iconfont icon-chepiao"/>
+        <view class="description">
+          已约车票
+        </view>
       </view>
-    </scroll-view>
+      <view class="bus-btn bus-record">
+        <view class="iconfont icon-a-lishijilubiaodanjishi"/>
+        <view class="description">
+          乘车记录
+        </view>
+      </view>
+    </view>
   </theme-config>
 </template>
 
 <script setup lang="ts">
-import { ThemeConfig, TitleBar, WList, WListItem } from "@/components";
+import { ThemeConfig, TitleBar } from "@/components";
 import "./index.scss";
+// import Taro from "@tarojs/taro";
 
-const busInfo = () => {
-  Taro.navigateTo({
-    url: "/pages/schoolbus/info/index"
-  });
-};
+// const busInfo = () => {
+//   Taro.navigateTo({
+//     url: "/pages/schoolbus/info/index"
+//   });
+// };
 
-const busTicket = () => {
-  Taro.navigateTo({
-    url: "/pages/schoolbus/ticket/index"
-  });
-};
+// const busTicket = () => {
+//   Taro.navigateTo({
+//     url: "/pages/schoolbus/ticket/index"
+//   });
+// };
 
-const busRecord = () => {
-  Taro.navigateTo({
-    url: "/pages/schoolbus/record/index"
-  });
-};
+// const busRecord = () => {
+//   Taro.navigateTo({
+//     url: "/pages/schoolbus/record/index"
+//   });
+// };
 </script>
