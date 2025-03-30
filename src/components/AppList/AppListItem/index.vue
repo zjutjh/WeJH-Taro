6365c3949f102162e50c589cdcff78c2b308d9b3
j10c <blyb1739@gmail.com>
2025-03-30 17:08:01 +0800

    fix(app-list): `require` 不匹配的一律置灰

--- src/components/AppList/AppListItem/index.vue
+++ src/components/AppList/AppListItem/index.vue
@@ -38,15 +38,20 @@ const iconClass = computed(() => {
   }
 });
 
-const isDisabled = ref(false);
-// 之后需要改动，目前zf和oauth的功能是等效的，因此zf和oauth有一个为true即可使用
-// 原来的代码是 if (requireActive.value === "zf" && !serviceStore.user.isBindZF)
-if (requireActive.value === "zf" && !serviceStore.user.isBindZF && !serviceStore.user.isBindOauth)
-  isDisabled.value = true;
-if (requireActive.value === "oauth" && !serviceStore.user.isBindOauth)
-  isDisabled.value = true;
-if (requireActive.value === "yxy" && !serviceStore.user.isBindYXY)
-  isDisabled.value = true;
+const isDisabled = computed(() => {
+  switch (requireActive.value) {
+    case "oauth":
+      return !serviceStore.user.isBindOauth;
+    case "zf":
+      return !serviceStore.user.isBindZF && !serviceStore.user.isBindOauth;
+    case "yxy":
+      return !serviceStore.user.isBindYXY;
+    case "null":
+      return false;
+    default:
+      return true;
+  }
+});
 
 async function appTaped() {
   if (isDisabled.value) {
