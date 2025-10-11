b7847f733d1ef307e04fbddb8a546224342d0c6d
折乙 <2332876536@qq.com>
2025-10-11 10:56:44 +0800

    chore(zjutjh-eslint): 配置开启prettier; zjutjh-eslint依赖移动到devDependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,12 @@
 import zjutjh from "@zjutjh/eslint-config";
 
-export default zjutjh({
-  overrides: {
-    vue: {
-      "vue/component-name-in-template-casing": [
-        "error",
-        "kebab-case",
-        {
-          registeredComponentsOnly: false
-        }
-      ]
-    }
+export default zjutjh(
+  {
+    prettier: true
   },
-});
+  {
+    rules: {
+      "unicorn/filename-case": "off"
+    }
+  }
+);
