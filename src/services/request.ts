102897a07572d4516bd08ee059eedcc5028128b6
Promise <i@xenlyra.cc>
2025-10-19 23:03:54 +0800

    fix(request): request 函数增加登录状态的判断

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,5 +1,9 @@
 import Taro from "@tarojs/taro";
 
+import { serviceStore } from "@/store";
+
+import { LoginByTaro } from "./services/authService";
+
 interface IResponse<T> {
   code: number;
   msg: string;
@@ -10,7 +14,11 @@ const globalConfig: Partial<Taro.request.Option> = {
   timeout: 12 * 1000
 };
 
-const request = <TData>(url: string, config: Omit<Taro.request.Option, "url">) => {
+const request = async <TData>(url: string, config: Omit<Taro.request.Option, "url">) => {
+  if (config.header && !config.header.Cookie) {
+    await LoginByTaro();
+    config.header.Cookie = serviceStore.sessionID;
+  }
   return Taro.request<IResponse<TData>>({
     ...globalConfig,
     url,
