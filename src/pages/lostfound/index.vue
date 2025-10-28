2d5711fb1746fa656afc4396624c46d1602a4d91
折乙 <2332876536@qq.com>
2025-10-28 19:20:37 +0800

    feat(lostfound/ContactMsg): ContactMsg封装为constant

--- src/pages/lostfound/index.vue
+++ src/pages/lostfound/index.vue
@@ -62,6 +62,7 @@ import { computed, ref } from "vue";
 
 import { Card, ContactMe, ThemeConfig, TitleBar, WSkeleton } from "@/components";
 import { helpText } from "@/constants/copywriting";
+import { contactMsg } from "@/constants/lostfound";
 import { useRequest } from "@/hooks";
 import { LostfoundService } from "@/services";
 import store, { serviceStore } from "@/store";
@@ -81,27 +82,6 @@ const selectMain = ref(serviceStore.lostfound.lastOpenMain || "全部");
 const isEmpty = ref(false);
 const helpContent = ref(helpText.lostfound);
 const isShowHelp = ref(false);
-
-const contactMsg = {
-  data: [
-    { content: "“For You”工程 朝晖校区 QQ: 2716709627" },
-    { content: "“For You”工程 屏峰校区 QQ: 3252819828" },
-    { content: "“For You”工程 莫干山校区 QQ: 3525146766" },
-    {
-      content: "学生事务大厅-朝晖: 综合楼一楼河畔旁",
-      extra: "学生事务大厅-朝晖\r\n电话: 88320868"
-    },
-    {
-      content: "学生事务大厅-屏峰: 西4和东15楼下",
-      extra: "学生事务大厅-屏峰\r\n东15电话: 85290858\r\n西4电话:85290880"
-    },
-    {
-      content: "学生事务大厅-莫干山: 德8德9连廊",
-      extra: "学生事务大厅-莫干山\r\n电话: (0571) 8881 3551"
-    }
-  ],
-  message: "点击查看相关业务服务组织的联系方式"
-};
 
 const { data: getKindsResponse } = useRequest(LostfoundService.getKindList, {
   onSuccess: (res) => {
