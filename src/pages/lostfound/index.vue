c7465640f3fabfc5942f019dc4b8526100fb6a3b
折乙 <2332876536@qq.com>
2025-10-28 15:16:12 +0800

    chore(lostFound/ForYou): 更新莫干山ForYou QQ号

--- src/pages/lostfound/index.vue
+++ src/pages/lostfound/index.vue
@@ -18,7 +18,7 @@
         <text
           v-for="item in mainList"
           :key="item"
-          :class="selectMain === item ? 'active': undefined"
+          :class="selectMain === item ? 'active' : undefined"
           @tap="() => handleSelectMain(item)"
         >
           {{ item }}
@@ -28,7 +28,7 @@
         <text
           v-for="item in kindList"
           :key="item"
-          :class="selectKind === item ? 'active': undefined"
+          :class="selectKind === item ? 'active' : undefined"
           @tap="() => handleSelectKind(item)"
         >
           {{ item }}
@@ -42,37 +42,33 @@
       @scrolltolower="handleScrollToBottom"
     >
       <view class="record-list">
-        <preview-card
-          v-for="item in recordList"
-          :key="item.id"
-          :source="item"
-        />
-        <w-skeleton v-if="loading" :style="{borderRadius: '8Px'}" />
+        <preview-card v-for="item in recordList" :key="item.id" :source="item" />
+        <w-skeleton v-if="loading" :style="{ borderRadius: '8Px' }" />
         <card v-else-if="!recordList.length && isEmpty">
           <text>该分类下暂无失物寻物记录</text>
         </card>
       </view>
     </scroll-view>
     <contact-me :data="contactMsg.data" :message="contactMsg.message" @show-help="setHelp" />
-    <w-modal
-      v-model:show="isShowHelp"
-      :content="`&emsp;&emsp;${helpContent}`"
-    />
+    <w-modal v-model:show="isShowHelp" :content="`&emsp;&emsp;${helpContent}`" />
   </theme-config>
 </template>
 
 <script setup lang="ts">
-import { ThemeConfig, TitleBar, Card, ContactMe, WSkeleton } from "@/components";
+import "./index.scss";
+
+import { omit } from "lodash-es";
+import { computed, ref } from "vue";
+
+import { Card, ContactMe, ThemeConfig, TitleBar, WSkeleton } from "@/components";
+import { helpText } from "@/constants/copywriting";
 import { useRequest } from "@/hooks";
 import { LostfoundService } from "@/services";
-import { LostfoundRecord } from "@/types/Lostfound";
-import { computed, ref } from "vue";
-import PreviewCard from "./PreviewCard/index.vue";
-import { omit } from "lodash-es";
-import "./index.scss";
 import store, { serviceStore } from "@/store";
+import { LostfoundRecord } from "@/types/Lostfound";
+
 import WModal from "../../components/Modal/index.vue";
-import { helpText } from "@/constants/copywriting";
+import PreviewCard from "./PreviewCard/index.vue";
 
 const currentPage = ref(0);
 const maxPage = ref(0);
@@ -81,7 +77,7 @@ const campusList = ref<string[]>(["屏峰", "朝晖", "莫干山"]);
 const selectKind = ref("全部");
 const selectCampus = ref(serviceStore.lostfound.lastOpenCampus || "屏峰");
 const mainList = ref<string[]>(["全部", "失物", "寻物"]);
-const selectMain = ref(serviceStore .lostfound.lastOpenMain || "全部");
+const selectMain = ref(serviceStore.lostfound.lastOpenMain || "全部");
 const isEmpty = ref(false);
 const helpContent = ref(helpText.lostfound);
 const isShowHelp = ref(false);
@@ -90,7 +86,7 @@ const contactMsg = {
   data: [
     { content: "“For You”工程 朝晖校区 QQ: 2716709627" },
     { content: "“For You”工程 屏峰校区 QQ: 3252819828" },
-    { content: "“For You”工程 莫干山校区 QQ: 2030219390" },
+    { content: "“For You”工程 莫干山校区 QQ: 3525146766" },
     {
       content: "学生事务大厅-朝晖: 综合楼一楼河畔旁",
       extra: "学生事务大厅-朝晖\r\n电话: 88320868"
@@ -107,41 +103,35 @@ const contactMsg = {
   message: "点击查看相关业务服务组织的联系方式"
 };
 
-const { data: getKindsResponse } = useRequest(
-  LostfoundService.getKindList, {
-    onSuccess: (res) => {
-      if (res.data.code !== 1) throw new Error(res.data.msg);
-    },
-    onError: (e: Error) => {
-      return `获取分类失败\r\n${e.message || "网络错误"}`;
-    }
+const { data: getKindsResponse } = useRequest(LostfoundService.getKindList, {
+  onSuccess: (res) => {
+    if (res.data.code !== 1) throw new Error(res.data.msg);
+  },
+  onError: (e: Error) => {
+    return `获取分类失败\r\n${e.message || "网络错误"}`;
   }
-);
+});
 
-const { loading, run } = useRequest(
-  LostfoundService.getRecords, {
-    defaultParams: {
-      campus: selectCampus.value,
-      page_num: currentPage.value + 1,
-      page_size: 10,
-      lost_or_found: ""
-    },
-    loadingDelay: 300,
-    onSuccess: (res) => {
-      if (res.data.code === 1) {
-        recordList.value = recordList.value?.concat(
-          res.data.data.data
-        );
-        if (recordList.value.length === 0) isEmpty.value = true;
-        maxPage.value = res.data.data.total_page_num;
-        currentPage.value++;
-      } else throw new Error(res.data.msg);
-    },
-    onError: (e: Error) => {
-      return `加载失物寻物信息失败\r\n${e.message || "网络错误"}`;
-    }
+const { loading, run } = useRequest(LostfoundService.getRecords, {
+  defaultParams: {
+    campus: selectCampus.value,
+    page_num: currentPage.value + 1,
+    page_size: 10,
+    lost_or_found: ""
+  },
+  loadingDelay: 300,
+  onSuccess: (res) => {
+    if (res.data.code === 1) {
+      recordList.value = recordList.value.concat(res.data.data.data);
+      if (recordList.value.length === 0) isEmpty.value = true;
+      maxPage.value = res.data.data.total_page_num;
+      currentPage.value++;
+    } else throw new Error(res.data.msg);
+  },
+  onError: (e: Error) => {
+    return `加载失物寻物信息失败\r\n${e.message || "网络错误"}`;
   }
-);
+});
 
 const getRecords = (data: {
   campus?: string;
@@ -151,13 +141,17 @@ const getRecords = (data: {
   lost_or_found?: string;
 }) => {
   isEmpty.value = false;
-  run(omit(data, [data.kind === "全部" ? "kind" : null, data.lost_or_found === "全部" ? "lost_or_found" : ""]));
+  run(
+    omit(data, [
+      ...(data.kind === "全部" ? ["kind"] : []),
+      ...(data.lost_or_found === "全部" ? ["lost_or_found"] : [])
+    ]) as typeof data
+  );
 };
 
 const kindList = computed<string[]>(() => [
   "全部",
-  ...(getKindsResponse.value?.data || [])
-    .map(item => item.kind_name)
+  ...(getKindsResponse.value?.data || []).map((item) => item.kind_name)
 ]);
 
 const handleSelectCampus = (campus: string) => {
@@ -208,7 +202,7 @@ const resetList = () => {
 
 const handleScrollToBottom = () => {
   if (loading.value || maxPage.value <= currentPage.value) return;
-  else getRecords({
+  getRecords({
     campus: selectCampus.value,
     kind: selectKind.value,
     page_num: currentPage.value + 1,
