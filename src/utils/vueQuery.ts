4f802d17a2bcbce97e09551450fb4f498945430f
Promise <i@xenlyra.cc>
2025-10-30 20:01:36 +0800

    feat: 添加持久化存储

--- src/utils/vueQuery.ts
+++ src/utils/vueQuery.ts
@@ -1,12 +1,26 @@
+import { experimental_createQueryPersister as createQueryPersister } from "@tanstack/query-persist-client-core";
 import { QueryClient, QueryClientConfig } from "@tanstack/vue-query";
 
+import { persistedStorage } from "./storage";
+
 const OneMinute = 60 * 1000;
+const OneDay = 24 * 60 * 60 * 1000;
 
 const globalQueryClientConfig: QueryClientConfig = {
   defaultOptions: {
     queries: {
       retry: 2,
-      staleTime: 5 * OneMinute
+      meta: {
+        persist: true
+      },
+      staleTime: 5 * OneMinute,
+      persister: createQueryPersister({
+        storage: persistedStorage,
+        maxAge: 7 * OneDay,
+        filters: {
+          predicate: (query) => Boolean(query.meta?.persist)
+        }
+      }).persisterFn
     }
   }
 };
