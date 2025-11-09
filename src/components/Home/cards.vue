570a60a9cc38a92206c142865c2b3e03b6990963
S2ltyF1sh <rojasgraves82@gmail.com>
2025-11-09 22:58:39 +0800

    refactor() :对相关代码Eslint修复

--- src/components/Home/cards.vue
+++ src/components/Home/cards.vue
@@ -1,29 +1,32 @@
 <script setup lang="ts">
-import { Fragment, computed, h, ref } from "vue";
+import { computed, Fragment, h, ref } from "vue";
+
 import { helpText } from "@/constants/copywriting";
 import { serviceStore } from "@/store";
-import LessonsTableQuickView from "../LessonsTableQuickView/index.vue";
+
+import ElectricityQuickView from "../ElectricityQuickView/index.vue";
 import ExamQuickView from "../ExamQuickView/index.vue";
-import SchoolCardQuickView from "../SchoolCardQuickView/index.vue";
-import ScoreQuickView from "../ScoreQuickView/index.vue";
+import LessonsTableQuickView from "../LessonsTableQuickView/index.vue";
 import LibraryQuickView from "../LibraryQuickView/index.vue";
 import WModal from "../Modal/index.vue";
-import ElectricityQuickView from "../ElectricityQuickView/index.vue";
+import SchoolCardQuickView from "../SchoolCardQuickView/index.vue";
+import ScoreQuickView from "../ScoreQuickView/index.vue";
 
 const helpContent = ref<string | undefined>(undefined);
 const isShowHelp = ref(false);
 
-const cards = () => h(
-  Fragment,
-  serviceStore.homecard.selected
-    .map(item => cardsMap.value[item])
-    .filter(item => item !== null)
-);
+const cards = () =>
+  h(
+    Fragment,
+    serviceStore.homecard.selected
+      .map((item) => cardsMap.value[item])
+      .filter((item) => item !== null)
+  );
 
 const isBindZf = computed(() => {
   // 之后需要改动，目前zf和oauth的功能是等效的，因此zf和oauth有一个为true即可使用
   // 原来的代码是 return (serviceStore.user.isBindZF);
-  return (serviceStore.user.isBindZF || serviceStore.user.isBindOauth);
+  return serviceStore.user.isBindZF || serviceStore.user.isBindOauth;
 });
 const isBindOauth = computed(() => {
   return serviceStore.user.isBindOauth;
@@ -33,32 +36,28 @@ const isBindYxy = computed(() => {
 });
 
 const cardsMap = computed(() => ({
-  "lessons-table-quick-view": isBindZf.value ? h(
-    LessonsTableQuickView, {
-      "onShowHelp": () => showHelp("lessons-table")
-    }
-  ) : null,
-  "exam-quick-view": isBindZf.value ? h(
-    ExamQuickView, {
-      "onShowHelp": () => showHelp("exam-card")
-    }
-  ) : null,
-  "school-card-quick-view": isBindYxy.value ? h(
-    SchoolCardQuickView, {
-      "onShowHelp": () => showHelp("school-card")
-    }
-  ) : null,
-  "score-quick-view": isBindZf.value ? h(
-    ScoreQuickView, {
-      "onShowHelp": () => showHelp("score-card")
-    }
-  ) : null,
-  "library-quick-view": isBindOauth.value ? h(
-    LibraryQuickView
-  ) : null,
-  "electricity-quick-view": isBindYxy.value ? h(
-    ElectricityQuickView
-  ) : null
+  "lessons-table-quick-view": isBindZf.value
+    ? h(LessonsTableQuickView, {
+        onShowHelp: () => showHelp("lessons-table")
+      })
+    : null,
+  "exam-quick-view": isBindZf.value
+    ? h(ExamQuickView, {
+        onShowHelp: () => showHelp("exam-card")
+      })
+    : null,
+  "school-card-quick-view": isBindYxy.value
+    ? h(SchoolCardQuickView, {
+        onShowHelp: () => showHelp("school-card")
+      })
+    : null,
+  "score-quick-view": isBindZf.value
+    ? h(ScoreQuickView, {
+        onShowHelp: () => showHelp("score-card")
+      })
+    : null,
+  "library-quick-view": isBindOauth.value ? h(LibraryQuickView) : null,
+  "electricity-quick-view": isBindYxy.value ? h(ElectricityQuickView) : null
 }));
 
 function showHelp(prop: string) {
@@ -68,13 +67,9 @@ function showHelp(prop: string) {
   else if (prop === "school-card") helpContent.value = helpText.schoolCard;
   else if (prop === "exam-card") helpContent.value = helpText.examCard;
 }
-
 </script>
 
 <template>
   <cards />
-  <w-modal
-    v-model:show="isShowHelp"
-    :content="`&emsp;&emsp;${helpContent}`"
-  />
+  <w-modal v-model:show="isShowHelp" :content="`&emsp;&emsp;${helpContent}`" />
 </template>
