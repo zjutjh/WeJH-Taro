092e67c8f5d820379a8b1df555c1aca06567ceff
j10c <blyb1739@gmail.com>
2025-11-29 00:44:39 +0800

    feat(monitor): 追加上报学号

--- src/plugins/aegis.ts
+++ src/plugins/aegis.ts
@@ -6,6 +6,7 @@ import { ServiceErrorCode } from "@/utils/request-error";
 import { IResponse } from "@/utils/request-next";
 
 const DEFAULT_ERROR_RES_CODE = -999999;
+const DEFAULT_UNI = "DEFAULT_UNI";
 
 const AEGIS_ENV_MAP = {
   development: Aegis.environment.development,
@@ -14,14 +15,15 @@ const AEGIS_ENV_MAP = {
 } satisfies Record<ImportMetaEnv["VITE_BUILD_ENV"], Environment>;
 
 /**
+ * 腾讯云可观测平台 aegis 日志上报
  * @see https://cloud.tencent.com/document/product/1464/58570
  */
 export const aegis = new Aegis({
-  id: import.meta.env.VITE_TENCENT_RUM_REPORT_KEY, // RUM 申请的上报 key
-  // TODO: 用户唯一 ID（可选）
-  // uin: "xxx", // 用户唯一 ID（可选）
-  reportApiSpeed: true, // 接口测速
-  spa: true, // 是否在小程序页面跳转时进行 PV 上报。
+  env: AEGIS_ENV_MAP[import.meta.env.VITE_BUILD_ENV],
+  id: import.meta.env.VITE_TENCENT_RUM_REPORT_KEY,
+  uin: DEFAULT_UNI, // 用户学号，请求到学号之前用默认值
+  reportApiSpeed: true,
+  spa: true,
   api: {
     retCodeHandler: (data: unknown) => {
       const code = get(data as IResponse<unknown>, "code", DEFAULT_ERROR_RES_CODE);
@@ -32,5 +34,8 @@ export const aegis = new Aegis({
       };
     }
   },
-  env: AEGIS_ENV_MAP[import.meta.env.VITE_BUILD_ENV]
+  // setData 和页面渲染性能相关，且上报频率较高，不需要监控
+  setDataReportConfig: {
+    disabled: true
+  }
 });
