f2ef23565ecce5ce0356567500b3031ad871b8d8
j10c <blyb1739@gmail.com>
2025-12-29 02:29:48 +0800

    refactor(bus): 班车分组逻辑调整

--- src/pages/school-bus/_components/bus-line-modal/index.vue
+++ src/pages/school-bus/_components/bus-line-modal/index.vue
@@ -2,7 +2,7 @@
   <w-modal v-model:show="show" title="全部路线" :class="styles['line-modal']">
     <view :class="styles['content']">
       <view
-        v-for="(lines, groupName) in props.lineList"
+        v-for="(busNameList, groupName) in group"
         :key="groupName"
         :class="styles['group-section']"
       >
@@ -12,8 +12,13 @@
         </view>
 
         <view :class="styles['chips-container']">
-          <view v-for="line in lines" :key="line" :class="styles['chip']" @tap="handleSelect(line)">
-            {{ line }}
+          <view
+            v-for="busName in busNameList"
+            :key="busName"
+            :class="styles['chip']"
+            @tap="handleSelect(busName)"
+          >
+            {{ busName }}
           </view>
         </view>
       </view>
@@ -22,28 +27,36 @@
 </template>
 
 <script setup lang="ts">
+import { groupBy, mapValues, uniq } from "lodash-es";
+import { computed } from "vue";
+
 import { WModal } from "@/components";
 
+import { useBusConfig } from "../../_hooks/use-bus-info";
+import { parseRouteName } from "../../_utils";
 import styles from "./index.module.scss";
-
-interface BusLinesMap {
-  [key: string]: string[];
-}
-
-const props = defineProps<{
-  lineList: BusLinesMap;
-}>();
 
 const show = defineModel<boolean>("show", {
   default: false
+});
+
+const { busConfig } = useBusConfig();
+
+const group = computed(() => {
+  const _group = groupBy(busConfig.value, (item) => {
+    const { start, end } = parseRouteName(item.name);
+    return `${[start, end].sort().join("、")} 之间往返`;
+  });
+
+  return mapValues(_group, (value) => uniq(value.map((_) => parseRouteName(_.name).busName)));
 });
 
 const emit = defineEmits<{
   (e: "select", line: string): void;
 }>();
 
-const handleSelect = (line: string) => {
-  emit("select", line);
+const handleSelect = (busName: string) => {
+  emit("select", busName);
   show.value = false;
 };
 </script>
--- src/pages/school-bus/_hooks/use-bus-info.ts
+++ src/pages/school-bus/_hooks/use-bus-info.ts
@@ -1,11 +1,10 @@
 import { useQuery } from "@tanstack/vue-query";
-import { computed, MaybeRef, unref } from "vue";
+import { groupBy } from "lodash-es";
+import { computed } from "vue";
 
 import { yxyServiceNext } from "@/services";
 import { QUERY_KEY } from "@/services/api/query-key";
-import { isPFCampus } from "@/utils/school-bus";
 
-import { BusRouteDetail } from "../_types";
 import { parseRouteName } from "../_utils";
 
 // TODO: 文件名修改
@@ -20,82 +19,15 @@ export const useBusConfig = () => {
 };
 
 /** 根据校区分类的线路名列表 Hook */
-export const useBusLineList = () => {
+export const useBusNameGroup = () => {
   const { busConfig, isLoading } = useBusConfig();
 
-  const busLineList = computed(() => {
-    const config = busConfig.value || [];
-    console.log("* config *", { config });
-
-    const groups = {
-      "朝晖-屏峰": new Set<string>(),
-      "朝晖-莫干山": new Set<string>(),
-      "屏峰-莫干山": new Set<string>(),
-      "": new Set<string>()
-    };
-
-    config.forEach((bus) => {
-      const { busName, start, end } = parseRouteName(bus.name);
-      const campuses = new Set([start, end]);
-
-      const hasPF = isPFCampus(start) || isPFCampus(end);
-      if (campuses.has("朝晖") && hasPF) {
-        groups["朝晖-屏峰"].add(busName);
-      } else if (campuses.has("朝晖") && campuses.has("莫干山")) {
-        groups["朝晖-莫干山"].add(busName);
-      } else if (hasPF && campuses.has("莫干山")) {
-        groups["屏峰-莫干山"].add(busName);
-      } else {
-        groups[""].add(busName);
-      }
-    });
-
-    return {
-      "朝晖-屏峰": Array.from(groups["朝晖-屏峰"]),
-      "朝晖-莫干山": Array.from(groups["朝晖-莫干山"]),
-      "屏峰-莫干山": Array.from(groups["屏峰-莫干山"]),
-      "": Array.from(groups[""])
-    };
-  });
-
-  return { busLineList, isLoading };
-};
-
-/**
- * 获取某条线路详情的 Hook
- * @param matcher 匹配条件
- */
-export const useBusRoute = (
-  matcher: MaybeRef<{
-    routeName: string;
-    start: string;
-    end: string;
-  }>
-) => {
-  const { busConfig } = useBusConfig();
-
-  const busRouteList = computed<BusRouteDetail[]>(() => {
-    const config = busConfig.value || [];
-    return config.map((bus) => {
-      const { busName, start, end } = parseRouteName(bus.name);
-
-      return {
-        busName,
-        start,
-        end,
-        stations: bus.stations.map((s) => ({ stationName: s }))
-      };
+  const group = computed(() => {
+    return groupBy(busConfig.value, (item) => {
+      const { start, end } = parseRouteName(item.name);
+      return [start, end].sort().join("-");
     });
   });
 
-  const route = computed(() => {
-    const m = unref(matcher);
-    return busRouteList.value.find(
-      (item) => item.busName === m.routeName && item.start === m.start && item.end === m.end
-    );
-  });
-
-  return {
-    route
-  };
+  return { group, isLoading };
 };
